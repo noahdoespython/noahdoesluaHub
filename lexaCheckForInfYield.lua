e6b7fa8be4832b6e19603b60ec5b3cb0f1eee92d
Noah Bate <92116978+noahdoespython@users.noreply.github.com>
2022-02-16 15:00:28 +0000

    Update lexaCheckForInfYield.lua

--- lexaCheckForInfYield.lua
+++ lexaCheckForInfYield.lua
@@ -1,20 +1,3 @@
-local txt = script.Parent:FindFirstChild("AuthencatingTxt")
-local Conf = txt.Parent:FindFirstChild("Confirmed")
-local passedCheck = "User passed Leka Check."
-local GUI = script.Parent.Parent
-
-Conf.MouseButton1Down:Connect(function()
-	if txt.Text == "leka.98a3771c1c194c20e81fd5fc3dc3a8dd" then
-		warn(passedCheck)
-		loadstring(game:HttpGet("", true))()
-		GUI:Destroy()
-	else
-		game.Players.LocalPlayer:Kick("Lexa Key Invalid!")
-		GUI:Destroy()
-	end
-
-end)
-
 local v = {
     "Enter key..",
     "MaxTextSize",
